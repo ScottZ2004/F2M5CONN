38228ccc7ee4f659bf46e4045bee399bae6fd793
Kipmevrouw <70896873+Kipmevrouw@users.noreply.github.com>
2021-09-27 14:30:26 +0200

    code goed

--- Code/sketch_sep27a/sketch_sep27a.ino
+++ Code/sketch_sep27a/sketch_sep27a.ino
@@ -50,12 +50,4 @@ void loop() {
   Serial.println(distanceCm);
   
   delay(1000);
-}void setup() {
-  // put your setup code here, to run once:
-
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
 }
